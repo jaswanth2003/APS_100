a935af581c4e0bc2bcdd921179333310692646bb
jaswanth2003 <jaswanthreddykanumukkala@gmail.com>
2023-04-28 22:09:58 +0530

    modified

---
+++ 5.Maximum_Subarray.py
@@ -0,0 +1,8 @@
+def maxSubArray(nums: List[int]) -> int:
+    max_sum = float('-inf')
+    curr_sum = 0
+    for num in nums:
+        curr_sum += num
+        max_sum = max(max_sum, curr_sum)
+        curr_sum = max(curr_sum, 0)
+    return max_sum
---
+++ 7.Palindromic_Substrings.py
@@ -0,0 +1,7 @@
+def countSubstrings(s: str) -> int:
+    count = 0
+    for i in range(len(s)):
+        for j in range(i, len(s)):
+            if s[i:j+1] == s[i:j+1][::-1]:
+                count += 1
+    return count
